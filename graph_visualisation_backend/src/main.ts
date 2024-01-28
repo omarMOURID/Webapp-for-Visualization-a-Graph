0edf8d1536a18bccdba3a9db7358c1001ce47d61
omarMOURID <mourid463@gmail.com>
2024-01-28 14:41:35 +0100

    enabling the global validation pipes

--- graph_visualisation_backend/src/main.ts
+++ graph_visualisation_backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  // Create an instance of the Nest application
   const app = await NestFactory.create(AppModule);
+
+  // Use global validation pipes for request payload validation
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Start the application and listen on port 4000
   await app.listen(4000);
 }
 bootstrap();
