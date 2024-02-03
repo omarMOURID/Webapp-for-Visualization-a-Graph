54324ebf77e1d0d43a24013b9abb680e5469011a
omarMOURID <mourid463@gmail.com>
2024-02-03 19:56:03 +0100

    Enhance queryGraphDto to include node name, pmcid, and sent_id for comprehensive data representation.

--- graph_visualisation_backend/src/graph/dto/query_graph.dto.ts
+++ graph_visualisation_backend/src/graph/dto/query_graph.dto.ts
@@ -1,4 +1,4 @@
-import { IsEnum, IsOptional } from 'class-validator';
+import { IsEnum, IsInt, IsNotEmpty, IsOptional, IsString, ValidateIf } from 'class-validator';
 import { Label, Relation } from '../graph.types';
 
 export class QueryGraphDto {
@@ -9,4 +9,19 @@ export class QueryGraphDto {
   @IsEnum(Relation, { each: true }) // Validate each relation in the array
   @IsOptional()
   relations: Relation[];
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  node: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ValidateIf(o => o.sentenceid)
+  pmcid: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  @IsOptional()
+  sentenceid: number;
 }
